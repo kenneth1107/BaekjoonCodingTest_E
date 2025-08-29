7d0ef4897c4df012847f7d769b41c48ca5c49c65
kenneth1107 <power6778@gmail.com>
2025-08-29 11:21:32 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+
+        if(0 < A && B < 10){
+            System.out.println(A/B);
+        }else{
+            System.out.println("0 < A && B < 10");
+        }
+    }
+}
