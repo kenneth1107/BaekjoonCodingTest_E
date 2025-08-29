a3979e9f1a732dd1bda883ccda4d9a92ba215cac
kenneth1107 <power6778@gmail.com>
2025-08-29 13:08:06 +0900

    [Bronze V] Title: 나머지, Time: 180 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if(2 <= A && A <= 10000 &&
+        2 <= B && B <= 10000 &&
+        2 <= C && C <= 10000 && ((A+B)%C == ((A%C) + (B%C))%C)
+        ||  2 <= A && A <= 10000 &&
+            2 <= B && B <= 10000 &&
+            2 <= C && C <= 10000 && ((A*B)%C == ((A%C) * (B%C))%C)) {
+            System.out.println((A+B)%C);
+            System.out.println(((A%C) + (B%C))%C);
+            System.out.println((A*B)%C);
+            System.out.println(((A%C) * (B%C))%C);
+
+        }else{
+            System.out.println("nothing");
+        }
+    }
+}
